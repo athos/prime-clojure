a0ff943f29ebfea9ee6035e6dea05a1a2ad82d67
OHTA Shogo <athos0220@gmail.com>
2014-11-08 09:17:22 +0900

    first commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to prime-clojure
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/prime_clojure/core_test.clj
@@ -0,0 +1,7 @@
+(ns prime-clojure.core-test
+  (:require [clojure.test :refer :all]
+            [prime-clojure.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
